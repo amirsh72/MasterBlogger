55192d7cdaefa88be28093f9233f8e175637a6e8
amir12 <j0h@example.com>
2022-08-25 11:40:22 +0430

    Comment Domin Implement and application layer

---
+++ MB.Domain/CommentAgg/Comment.cs
@@ -0,0 +1,29 @@
+﻿using MB.Domain.ArticleAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.Domain.CommentAgg
+{
+    public class Comment
+    {
+        public long Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string message { get; private set; }
+        public int status { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public long ArticleId { get; private set; }
+        public Article article { get; private set; }
+
+        public Comment(string name, string email, string message, long articleId)
+        {
+            Name = name;
+            Email = email;
+            this.message = message;
+            ArticleId = articleId;
+        }
+    }
+}
